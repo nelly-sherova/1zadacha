826da910125fd1e1d54ab4177fbb6726d02e9dcc
unknown <lvova.nikol@mail.ru>
2020-03-22 12:56:45 +0300

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double sg = Math.Pow((a*b),0.5);
+            Console.WriteLine($"Среднее геометрическое: {sg} ");
+        }
+    }
+}
